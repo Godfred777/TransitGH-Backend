88f8604a5c2061edaad781decc124a7f974a2abc
Godfred Opintan <godfredopintan777@gmail.com>
2026-01-22 10:03:38 +0000

    feat: implement PrismaService with PostgreSQL connection setup

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma/client';
+import {PrismaPg} from '@prisma/adapter-pg'
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService {
+    constructor() {
+        const connectionString = process.env.DATABASE_URL;
+        const prismaPg = new PrismaPg({connectionString});
+        const prisma = new PrismaClient({
+            adapter: prismaPg,
+        });
+    }
+}
