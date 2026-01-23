e116b72fcb4c3962fe0eaa45f6ce39bc80ef2a33
Godfred Opintan <godfredopintan777@gmail.com>
2026-01-23 13:34:55 +0000

    refactor: remove unused User interface import from UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { Prisma } from 'generated/prisma/client';
-import { User } from '../interfaces/users.interfaces';
-
 
 @Injectable()
 export class UsersService {
