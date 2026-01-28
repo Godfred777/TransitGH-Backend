8fb3a23788da1ef2c6dfcf95e8c5598f6fc2c689
Godfred Opintan <godfredopintan777@gmail.com>
2026-01-28 13:08:05 +0000

    feat: add PrismaService to TripModule providers for enhanced database interaction

--- src/trip/trip.module.ts
+++ src/trip/trip.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TripService } from './trip.service';
 import { TripController } from './trip.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [TripService],
-  controllers: [TripController]
+  providers: [TripService, PrismaService],
+  controllers: [TripController],
+  exports: [TripService],
 })
 export class TripModule {}
