b2ef33ebb3c941be75278a3d9a45835696a492d5
Godfred Opintan <godfredopintan777@gmail.com>
2026-01-28 14:18:49 +0000

    feat: Exposed StopsService methods with exposed endpoints

--- src/stops/stops.controller.ts
+++ src/stops/stops.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete} from '@nestjs/common';
+import { StopsService } from './stops.service';
 
 @Controller('stops')
-export class StopsController {}
+export class StopsController {
+    constructor(private readonly stopsService: StopsService) {}
+
+    @Post()
+    async create(@Body() data: any) {
+        return this.stopsService.createStop(data);
+    }
+
+    @Get()
+    async findAll() {
+        return this.stopsService.getAllStops();
+    }
+
+    @Patch(':id/location')
+    async updateLocation(
+        @Param('id') id: number,
+        @Body('latitude') latitude: number,
+        @Body('longitude') longitude: number,
+    ) {
+        return this.stopsService.updateStopLocation(id, latitude, longitude);
+    }
+}
