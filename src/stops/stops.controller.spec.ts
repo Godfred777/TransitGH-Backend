6ad6e778f6fac2c57cf078e53350e570cbcb2e47
Godfred <godfredopintan777@gmail.com>
2026-04-02 11:38:56 +0000

    test: Add unit tests for create, findAll, and updateLocation methods in StopsController

--- src/stops/stops.controller.spec.ts
+++ src/stops/stops.controller.spec.ts
@@ -15,4 +15,34 @@ describe('StopsController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('create', () => {
+    it('should create a stop', async () => {
+      const data = { name: 'Test Stop', latitude: 40.7128, longitude: -74.0060 };
+      const result = { id: 1, ...data };
+      jest.spyOn(controller['stopsService'], 'createStop').mockResolvedValue(result);
+
+      expect(await controller.create(data)).toEqual(result);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of stops', async () => {
+      const result = [{ id: 1, name: 'Test Stop', latitude: 40.7128, longitude: -74.0060 }];
+      jest.spyOn(controller['stopsService'], 'getAllStops').mockResolvedValue(result);
+
+      expect(await controller.findAll()).toEqual(result);
+    });
+  });
+
+  describe('updateLocation', () => {
+    it('should update the location of a stop', async () => {
+      const id = 1;
+      const latitude = 40.7128;
+      const longitude = -74.0060;
+      const result = { id, name: 'Test Stop', latitude, longitude };
+      jest.spyOn(controller['stopsService'], 'updateStopLocation').mockResolvedValue(result);
+      expect(await controller.updateLocation(id, latitude, longitude)).toEqual(result);
+    });
+  });
 });
